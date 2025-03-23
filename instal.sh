56ba5e2642c81a6daf4644e0f05c7873236969fb
fixploit03 <164528883+fixploit03@users.noreply.github.com>
2025-03-24 06:39:28 +0700

    Update instal.sh

--- instal.sh
+++ instal.sh
@@ -1,34 +1,63 @@
 #!/bin/bash
-# [bash-completion.sh]
-# Skrip untuk mengaktifkan bash completion di shell interaktif.
-# Dibuat oleh: fixploit03
-# Github: https://github.com/fixploit03/bash-completion
+#..............: instal.sh
+#..............: Dibuat oleh: fixploit03
+#..............: Github: https://github.com/fixploit03/bash-completion
 
 if [[ $EUID -ne 0 ]]; then
-	echo "Skrip ini harus dijalankan sebagai root!"
+	echo "[-] Skrip ini harus dijalankan sebagai root!"
 fi
 
-read -p "Apakah Anda ingin mengaktifkan bash-completion [Y/n]: " nanya
+clear
+echo "---------------------------------------------------------------------------------------------"
+echo "|                                                                                           |"
+echo "| Script Bash untuk mengaktifkan fitur bash-completion pada shell Bash (Bourne Again Shell) |"
+echo "| Dibuat oleh: Rofi (Fixploit03)                                                            |"
+echo "| Github: https://github.com/fixploit03/bash-completion                                     |"
+echo "|                                                                                           |"
+echo "---------------------------------------------------------------------------------------------"
+echo ""
+
+read -p "[#] Apakah Anda ingin mengaktifkan bash-completion [Y/n]: " nanya
 
 if [[ "${nanya}" == "Y" || "${nanya}" == "y" ]]; then
+
+	gagal=()
+
+	echo "[*] Mengaktifkan bash-completion..."
         # File
         file_bash_completion="bash-completion.sh"
         file_bashrc=/etc/bash.bashrc
 
         # Kondisi jika file bashrc tidak ditemukan 
         if [[ ! -f "${file_bashrc}" ]]; then
-	        echo "[-] File ${file} tidak ditemukan. bash-completion gagal diaktifkan."
+	        echo "[-] File '${file}' tidak ditemukan. bash-completion gagal diaktifkan."
         	exit 1
         fi
 
         echo "" >> "${file_bashrc}"
+	if [[ $? -ne 0 ]]; then
+		gagal+=("error")
+ 	fi
+  
         cat "${file_bash_completion}" >> "${file_bashrc}"
-        echo "[+] bash-completion berhasil diaktifkan."
-        exit 0
+	if [[ $? -ne 0 ]]; then
+		gagal+=("error")
+ 	fi
+
+  	if [[ "${#gagal[@]}" -eq 0 ]]; then
+  		echo "[+] bash-completion berhasil diaktifkan."
+      		exit 0
+   	else
+		echo "[-] bash-completion gagal diaktifkan."
+  		exit 1
+    	fi
+      
 elif [[ "${nanya}" == "N" || "${nanya}" == "n" ]]; then
-        echo "[*] Semoga harimu menyenangkan ^_^"
+        echo "[*] Bye..."
 	exit 0
  else
         echo "[-] Masukkan tidak valid. Harap masukkan 'Y/n'."
 	exit 1
  fi
+
+# - END -
